29fd2db4c40761cd2e3cbeda903c06a986c6072f
SS79418 <folarin.soboyejo@kroger.com>
2019-05-09 17:17:24 -0400

    first commit

---
+++ process.py
@@ -0,0 +1,129 @@
+# Code adapted from: https://towardsdatascience.com/build-it-yourself-chatbot-api-with-keras-tensorflow-model-f6d75ce957a5
+# https://towardsdatascience.com/@andrejusb - Andrejus Baranovskis
+import nltk
+# nltk.download('punkt')
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+import numpy as np
+import pandas as pd
+import pickle
+import random
+from gensim.test.utils import common_texts
+from gensim.test.utils import datapath
+from gensim.models.fasttext import FastText, load_facebook_model
+# , load_facebook_vector
+# things we need for Tensorflow
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation, Dropout
+# from keras.optimizers import SGD
+
+# import our chat-bot intents file
+import json
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+words = []
+classes = []
+documents = []
+ignore_words = ['?']
+# loop through each sentence in our intents patterns
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # tokenize each word in the sentence
+        w = nltk.word_tokenize(pattern)
+        # add to our words list
+        words.extend(w)
+        # add to documents in our corpus
+        documents.append((w, intent['tag']))
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+# stem and lower each word and remove duplicates
+words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+# sort classes
+classes = sorted(list(set(classes)))
+# documents = combination between patterns and intents
+print (len(documents), "documents")
+# classes = intents
+print (len(classes), "classes", classes)
+# words = all words, vocabulary
+print (len(words), "unique stemmed words", words)
+
+model = FastText(size=4, window=3, min_count=1, sentences=common_texts, iter=10)
+print(model.wv['computer'])
+print(model.wv.most_similar("computer"))
+print(model.wv.similarity('compute', 'computer'))
+
+
+# model2 = load_facebook_model("wiki.en.bin")
+# # model2 = FastText.load_facebook_model('wiki.en.bin')
+# print(model2.wv['computer'])
+# print(model2.wv.most_similar("computer"))
+# print(model2.wv.similarity('compute', 'computer'))
+
+# create train and test lists. X - patterns, Y - intents
+# use bow for word embeddings or numericalization
+# ToDo use FastText for word embeddings
+
+# create our training data
+training = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+# training set, bag of words for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # stem each word - create base word, in attempt to represent related words
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    print("bag ", bag)
+    print("output_row ", output_row)
+    training.append([bag, output_row])
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+# create train and test lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+# ToDo - use PyTorch instead
+# # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# # equal to number of intents to predict output intent with softmax
+# model = Sequential()
+# model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(len(train_y[0]), activation='softmax'))
+#
+# # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# # Fit the model
+# model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+
+# p = bow("Load blood pessure for patient", words)
+# print (p)
+# print (classes)
+
+# inputvar = pd.DataFrame([p], dtype=float, index=['input'])
+#
+# print(model.predict(inputvar))
+#
+# # save model to file
+# pickle.dump(model, open("chatme-model.pkl", "wb"))
+#
+# # save all of our data structures
+# pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "chatme-data.pkl", "wb" ) )
